0b862576c10d4707dc274e39883bdb2217f6f293
Sam Radford <smr1292@yahoo.com>
2017-12-30 10:45:17 -0500

    Recipes java has working expandable list with the categories.  Headings inflate and deflate properly.

--- app/src/main/java/radfordsm2/androidrecipebook/Recipes.java
+++ app/src/main/java/radfordsm2/androidrecipebook/Recipes.java
@@ -3,17 +3,149 @@ package radfordsm2.androidrecipebook;
 import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ExpandableListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class Recipes extends AppCompatActivity {
+
+    private ExpandableListAdapter listAdapter;
+    private ExpandableListView expListView;
+    private List<String> listDataHeader;
+    private HashMap<String, List<String>> listDataChild;
+    private List<String> breakfast, appetizers, soups, salads, sides;
+    private List<String> main, desserts, drinks, breads, snacks;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipes);
+
+        // get the listview
+        expListView = (ExpandableListView) findViewById(R.id.recipes_accordion);
+
+        // preparing list data
+        prepareListData();
+        prepareListChildren();
+
+        listAdapter = new ExpandableListAdapter(this, listDataHeader, listDataChild);
+
+        // setting list adapter
+        expListView.setAdapter(listAdapter);
+
+        // Listview Group click listener
+        expListView.setOnGroupClickListener(new ExpandableListView.OnGroupClickListener() {
+
+            @Override
+            public boolean onGroupClick(ExpandableListView parent, View v,
+                                        int groupPosition, long id) {
+                // Toast.makeText(getApplicationContext(),
+                // "Group Clicked " + listDataHeader.get(groupPosition),
+                // Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        });
+
+        // Listview Group expanded listener
+        expListView.setOnGroupExpandListener(new ExpandableListView.OnGroupExpandListener() {
+
+            @Override
+            public void onGroupExpand(int groupPosition) {
+                Toast.makeText(getApplicationContext(),
+                        listDataHeader.get(groupPosition) + " Expanded",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        // Listview Group collasped listener
+        expListView.setOnGroupCollapseListener(new ExpandableListView.OnGroupCollapseListener() {
+
+            @Override
+            public void onGroupCollapse(int groupPosition) {
+                Toast.makeText(getApplicationContext(),
+                        listDataHeader.get(groupPosition) + " Collapsed",
+                        Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        // Listview on child click listener
+        expListView.setOnChildClickListener(new ExpandableListView.OnChildClickListener() {
+
+            @Override
+            public boolean onChildClick(ExpandableListView parent, View v,
+                                        int groupPosition, int childPosition, long id) {
+                // TODO Auto-generated method stub
+                Toast.makeText(
+                        getApplicationContext(),
+                        listDataHeader.get(groupPosition)
+                                + " : "
+                                + listDataChild.get(
+                                listDataHeader.get(groupPosition)).get(
+                                childPosition), Toast.LENGTH_SHORT)
+                        .show();
+                return false;
+            }
+        });
     }
+
 
     @Override
     public void onBackPressed(){
         NavUtils.navigateUpFromSameTask(this);
+    }
+
+    private void prepareListData() {
+
+        listDataHeader = new ArrayList<String>();
+        listDataChild = new HashMap<String,List<String>>();
+
+        // Adding child data
+        listDataHeader.add("Breakfast");
+        listDataHeader.add("Appetizers");
+        listDataHeader.add("Soups");
+        listDataHeader.add("Salads");
+        listDataHeader.add("Sides");
+        listDataHeader.add("Main Dishes");
+        listDataHeader.add("Desserts");
+        listDataHeader.add("Drinks");
+        listDataHeader.add("Breads and Pastries");
+        listDataHeader.add("Snacks");
+
+        breakfast = new ArrayList<String>();
+        appetizers = new ArrayList<String>();
+        soups = new ArrayList<String>();
+        salads = new ArrayList<String>();
+        sides = new ArrayList<String>();
+        main = new ArrayList<String>();
+        desserts = new ArrayList<String>();
+        drinks = new ArrayList<String>();
+        breads = new ArrayList<String>();
+        snacks = new ArrayList<String>();
+    }
+
+
+    private void prepareListChildren() {
+
+        breakfast.add("Omeltte");
+        salads.add("Chicken Ceasar Salad");
+        main.add("Crock Pot Roast");
+        main.add("Chicken Parmesan");
+        main.add("Double Stack");
+
+        listDataChild.put(listDataHeader.get(0), breakfast);
+        listDataChild.put(listDataHeader.get(1), appetizers);
+        listDataChild.put(listDataHeader.get(2), soups);
+        listDataChild.put(listDataHeader.get(3), salads);
+        listDataChild.put(listDataHeader.get(4), sides);
+        listDataChild.put(listDataHeader.get(5), main);
+        listDataChild.put(listDataHeader.get(6), desserts);
+        listDataChild.put(listDataHeader.get(7), drinks);
+        listDataChild.put(listDataHeader.get(8), breads);
+        listDataChild.put(listDataHeader.get(9), snacks);
     }
 }
